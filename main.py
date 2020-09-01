3b3710825997bd223ed12801c5b608510bdb544f
wiersmahomescho <wiersmahomeschooling@gmail.com>
2020-09-01 21:41:31 +0000

    ADDED MOAR COOOOOOOODE.

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+
 #import necessary behaviors
 import time
 import sys
 import os
 
+#presettings
+textspeed = 2
 
 #title "screen"
 print("Loading...")
 print("")
 
-time.sleep(3)
+time.sleep(textspeed)
 
 print("Welcome to VIRUS SIMULATOR V.1.0")
 print("")
+
+time.sleep(textspeed)
 
 #game setup code
 print("Select Gamemode...")
@@ -19,18 +24,36 @@ print("")
 
 print("1: Singleplayer")
 print("2: Multiplayer")
+print("3: Settings")
+print("4: Never mind, i quit.")
 print("")
 
-gamemode=input("Gamemode: ")
+gamemode = input("Gamemode: ")
 
 if gamemode == "1":
-  print("Choose gametype")
+  time.sleep(textspeed)
+  print("Choose gametype...")
   print("")
   print("1: Quick Game, Jump Right into the Action.")
   print("2: Custom. Change EVERY aspect of the game.")
   print("")
   gametype=input("Gametype: ")
   if gametype == "1":
+    time.sleep(textspeed)
     os.system('python quickgame.py')
 
 elif gamemode == "2":
+  time.sleep(textspeed)
+  print("multiplayer coming soon.")
+  print("goodbye. :)")
+  print("")
+  sys.exit
+
+elif gamemode == "3":
+  time.sleep(textspeed)
+  os.system('python settings.py')
+
+elif gamemode == "4":
+  print("Bye Bye")
+  sys.exit
+
---
+++ quickgame.py
@@ -0,0 +1,7 @@
+import random
+import time
+import os
+import sys
+
+age = random.randint(1,101)
+birthmonth = random.randint(1,13)
